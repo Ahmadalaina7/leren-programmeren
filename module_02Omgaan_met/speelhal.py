59fe7f4d2c880d4b7eae8f641b36c98f40c8b10b
Ahmad0118 <aalaina@student.scalda.nl>
2022-09-08 11:30:28 +0200

    .

---
+++ module_02Omgaan_met/speelhal.py
@@ -0,0 +1,8 @@
+ticket = 7.45
+vr = 0.37 * 9
+
+persons = int(input("met hoe veel personen ben je "))
+
+totaalprijs = round( (persons * ticket) + (persons * vr) )
+
+print ('Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar' , totaalprijs)
