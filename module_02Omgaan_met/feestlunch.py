91214234ce475d712d33b283509366229ffecd82
Ahmad0118 <aalaina@student.scalda.nl>
2022-09-08 13:08:04 +0200

    person opdracht

--- module_02Omgaan_met/feestlunch.py
+++ module_02Omgaan_met/feestlunch.py
@@ -4,4 +4,4 @@ korting = 0.5
 
 prijs = (((croissant * 17 ) + stokbrood * 2) - korting * 3)
 
-print('De feestlunch kost je bij de bakker voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!' , prijs)
+print('De feestlunch kost je bij de bakker voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!', prijs)
