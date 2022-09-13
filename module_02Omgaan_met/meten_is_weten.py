d86427b4fbca0c2c50ade5cb3ee5fe025429e628
Ahmad0118 <aalaina@student.scalda.nl>
2022-09-13 10:07:27 +0200

    opdracht meten is weten

--- module_02Omgaan_met/meten_is_weten.py
+++ module_02Omgaan_met/meten_is_weten.py
@@ -1,4 +1,11 @@
 a = input("voer de eerste geheel getal in ? ")
 b = input("voer de tweede geheel getal in ? ")
-if b > a:
-  print("max")
+if b < a:
+   print("a is het grootste getal: gevolgd door de waarde van max")
+
+elif a < b:
+   print("a is het kleinste getal gevolgd door de waarde van min")
+
+else : 
+   a = b
+   print("a en b zijn even groot")
