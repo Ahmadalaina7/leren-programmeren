75a71c6efd6a4e825e4ccddc37b8f2e1e1687bd9
Ahmad0118 <aalaina@student.scalda.nl>
2022-09-13 11:29:38 +0200

    kaas_opdracht

---
+++ module_02Omgaan_met/kaas_opdracht.py
@@ -0,0 +1,42 @@
+geel = input("is de kaas geel ? (ja/nee) ")
+if geel == "nee":
+    schimmel = input("heeft de kaas blauwe schimmel?(ja/Nee) ")
+
+    if schimmel == "ja":
+        korst = input("heeft de kaas een korst? (ja/Nee) ")
+
+        if korst == "ja":
+            print("Blue de Rochbaron")
+
+        else:
+            print("Foume d'Ambert")
+
+    else:
+        korst1 = input("heeft de kaas een korst? (ja/nee) ")
+
+        if korst1 == "ja":
+            print("Camembert")
+
+        else:
+            print("Mozzarella")
+
+if geel == "ja":
+    gaten = input("Zitten er gaten in? (ja/nee) ")
+
+    if gaten == "ja":
+        duur = input("Is de kaas belachelijk duur? (ja/Nee) ")
+
+        if duur == "ja":
+            print("Emmenthaler")
+
+        else:
+            print("Leerdammer")
+
+    else:
+        steen = input("Is de kaas hard als steen? (ja/Nee) ")
+
+        if steen == "ja":
+            print("Parmigiano Reggiano")
+
+        else:
+            print("Goudse kaas")
