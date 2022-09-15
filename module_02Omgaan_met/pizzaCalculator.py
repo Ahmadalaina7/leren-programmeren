8f020ccd8efc3b06908eaf8a1242ae950ca26178
Ahmad0118 <aalaina@student.scalda.nl>
2022-09-15 11:49:27 +0200

    pizzacalculator aangepast

--- module_02Omgaan_met/pizzaCalculator.py
+++ module_02Omgaan_met/pizzaCalculator.py
@@ -9,11 +9,28 @@ small = 7.99
 medium = 8.50
 large = 10.00
 
-small_pizza_size = int(input("Hoe veel small pizza's wilt uw hebben?"))
-medium_pizza_size = int(input("Hoe veel medium pizza's wilt uw hebben?"))
-large_pizza_size = int(input("Hoe veel large pizza's wilt uw hebben?"))
+var = 0
+while var == 0:
+    try : 
+        small_pizza_size = int(input("Hoe veel small pizza's wilt uw hebben? "))
+        var += 1
+    except :
+        print("vul een heel getal in")
+while var == 1:
+    try :
+         medium_pizza_size = int(input("Hoe veel medium pizza's wilt uw hebben? "))
+         var += 1
+    except :
+        print("vul een heel getal in")
+while var == 2: 
+    try :
+         large_pizza_size = int(input("Hoe veel large pizza's wilt uw hebben? "))
+         var += 1
+    except :
+        print("vul een heel getal in")
 
-totaal = small_pizza_size * small + medium_pizza_size * medium + large_pizza_size * large
+
+    totaal = round(small_pizza_size * small + medium_pizza_size * medium + large_pizza_size * large , 2)
 
 print(f''' uw heeft {small_pizza_size} small pizza's, {medium_pizza_size} medium pizza's, {large_pizza_size} large pizza's
-besteld, {totaal} bedrag is euro ''' )
+besteld, €{totaal} euro ''' )
