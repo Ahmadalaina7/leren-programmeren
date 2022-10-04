02df9ca6a45249b0b246c9be2ae5a8fa9d50d45e
Ahmad0118 <aalaina@student.scalda.nl>
2022-10-04 14:20:37 +0200

    example6.py

---
+++ module_03keeropKeer/for_ever_young/robotarm-python-2021-main/example6.py
@@ -0,0 +1,16 @@
+from shutil import move
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 6')
+robotArm.speed = 3
+# Jouw python instructies zet je vanaf hier:
+for i in range(0, 7):
+    robotArm.moveRight()
+for i in range(0, 8):
+    robotArm.grab()
+    robotArm.moveRight()
+    robotArm.drop()
+    for x in range(0, 2):
+        robotArm.moveLeft()
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
