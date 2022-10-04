f1f3f455cfb42b2eac130c707cba0dce84a73fb9
Ahmad0118 <aalaina@student.scalda.nl>
2022-10-04 13:47:25 +0200

    example.py , example2.py

--- module_03keeropKeer/for_ever_young/robotarm-python-2021-main/example.py
+++ module_03keeropKeer/for_ever_young/robotarm-python-2021-main/example.py
@@ -1,8 +1,12 @@
+from shutil import move
 from RobotArm import RobotArm
 
 robotArm = RobotArm('exercise 1')
 
 # Jouw python instructies zet je vanaf hier:
-
+robotArm.moveRight()
+robotArm.grab()
+robotArm.moveLeft()
+robotArm.drop()
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
---
+++ module_03keeropKeer/for_ever_young/robotarm-python-2021-main/example2.py
@@ -0,0 +1,24 @@
+from shutil import move
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 2')
+
+# Jouw python instructies zet je vanaf hier:
+robotArm.grab()
+for count in range(0,9):
+    robotArm.moveRight()
+robotArm.drop()
+for count in range(0,5):
+    robotArm.moveLeft()
+robotArm.grab()
+for count in range (0,5):
+    robotArm.moveRight()
+robotArm.drop()
+robotArm.moveLeft()
+robotArm.moveLeft()
+robotArm.grab()
+robotArm.moveRight()
+robotArm.moveRight()
+robotArm.drop()
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
