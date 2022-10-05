c2e8199408b46ed3d27976bc08014b852cd0a45e
Ahmad0118 <aalaina@student.scalda.nl>
2022-10-05 14:02:26 +0200

    quit.py

---
+++ module_03keeropKeer/quit.py
@@ -0,0 +1,11 @@
+
+optellen = 1
+while True :
+    quit = input("wilt u quit in vullen ? ").lower()
+    if quit == "quit": 
+        break
+        
+    else:
+        print(f"{optellen} voer quit in ! ")
+
+    optellen += 1
