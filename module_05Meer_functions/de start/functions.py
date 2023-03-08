4290c086c47a884c40ffafb7e3d0bfd111cd9f0f
Ahmad0118 <aalaina@student.scalda.nl>
2023-03-08 13:54:00 +0100

    delen met vrienden opdracht de functies verder gevuld

--- module_05Meer_functions/de start/functions.py
+++ module_05Meer_functions/de start/functions.py
@@ -6,25 +6,29 @@ from data import COST_FOOD_HORSE_COPPER_PER_DAY
 
 ##################### M04.D02.O2 #####################
 
-def copper2silver(amount:int) -> float:
+
+def copper2silver(amount: int) -> float:
     zilver = amount / 10
     return float(zilver)
-    
 
-def silver2gold(amount:int) -> float:
+
+def silver2gold(amount: int) -> float:
     goud = amount / 5
     return float(goud)
 
-def copper2gold(amount:int) -> float:
+
+def copper2gold(amount: int) -> float:
     silver = copper2silver(amount)
     goud = silver2gold(silver)
     return float(goud)
 
-def platinum2gold(amount:int) -> float:
+
+def platinum2gold(amount: int) -> float:
     platinum = amount * 25
     return (platinum)
 
-def getPersonCashInGold(personCash:dict) -> float:
+
+def getPersonCashInGold(personCash: dict) -> float:
     platinumIngoud = platinum2gold(personCash['platinum'])
     goud = personCash['gold']
     silverIngoud = silver2gold(personCash['silver'])
@@ -34,108 +38,120 @@ def getPersonCashInGold(personCash:dict) -> float:
 
 ##################### M04.D02.O4 #####################
 
-def getJourneyFoodCostsInGold(people:int, horses:int) -> float:
-    FOODCOSTE_HUMAN_COPPER =  (people*COST_FOOD_HUMAN_COPPER_PER_DAY)*JOURNEY_IN_DAYS
-    FOODCOSTE_HORSE_COPPER= (horses*COST_FOOD_HORSE_COPPER_PER_DAY)*JOURNEY_IN_DAYS
-    TOTAAL_IN_COPPER= FOODCOSTE_HORSE_COPPER + FOODCOSTE_HUMAN_COPPER
-    GOUD= copper2gold(TOTAAL_IN_COPPER)
-    return   round(GOUD,2)
-    
+
+def getJourneyFoodCostsInGold(people: int, horses: int) -> float:
+    FOODCOSTE_HUMAN_COPPER = (
+        people*COST_FOOD_HUMAN_COPPER_PER_DAY)*JOURNEY_IN_DAYS
+    FOODCOSTE_HORSE_COPPER = (
+        horses*COST_FOOD_HORSE_COPPER_PER_DAY)*JOURNEY_IN_DAYS
+    TOTAAL_IN_COPPER = FOODCOSTE_HORSE_COPPER + FOODCOSTE_HUMAN_COPPER
+    GOUD = copper2gold(TOTAAL_IN_COPPER)
+    return round(GOUD, 2)
+
 ##################### M04.D02.O5 #####################
 
-def getFromListByKeyIs(list:list, key:str, value:any) -> list:
+
+def getFromListByKeyIs(list: list, key: str, value: any) -> list:
     naam = []
     for i in list:
         if key in i and i[key] == value:
             naam.append(i)
     return naam
 
-def getAdventuringPeople(people:list) -> list:
-    pass
 
-def getShareWithFriends(friends:list) -> int:
-    pass
+def getAdventuringPeople(people: list) -> list:
+    return getFromListByKeyIs(people, 'adventuring', True)
 
-def getAdventuringFriends(friends:list) -> list:
-    pass
+
+def getShareWithFriends(friends: list) -> int:
+    return getFromListByKeyIs(friends, 'shareWith', True)
+
+
+def getAdventuringFriends(friends: list) -> list:
+   new_list = []
+   for dict in getAdventuringPeople(friends):
+        if dict in getShareWithFriends(friends):
+            new_list.append(dict)
+        return new_list
 
 ##################### M04.D02.O6 #####################
 
-def getNumberOfHorsesNeeded(people:int) -> int:
+def getNumberOfHorsesNeeded(people: int) -> int:
     pass
 
-def getNumberOfTentsNeeded(people:int) -> int:
+def getNumberOfTentsNeeded(people: int) -> int:
     pass
 
-def getTotalRentalCost(horses:int, tents:int) -> float:
+def getTotalRentalCost(horses: int, tents:int) -> float:
     pass
 
 ##################### M04.D02.O7 #####################
 
-def getItemsAsText(items:list) -> str:
+def getItemsAsText(items: list) -> str:
     pass
 
-def getItemsValueInGold(items:list) -> float:
+def getItemsValueInGold(items: list) -> float:
     pass
 
 ##################### M04.D02.O8 #####################
 
-def getCashInGoldFromPeople(people:list) -> float:
+def getCashInGoldFromPeople(people: list) -> float:
     pass
 
 ##################### M04.D02.O9 #####################
 
-def getInterestingInvestors(investors:list) -> list:
+def getInterestingInvestors(investors: list) -> list:
     pass
 
-def getAdventuringInvestors(investors:list) -> list:
+def getAdventuringInvestors(investors: list) -> list:
     pass
 
-def getTotalInvestorsCosts(investors:list, gear:list) -> float:
+def getTotalInvestorsCosts(investors: list, gear:list) -> float:
     pass
 
 ##################### M04.D02.O10 #####################
 
-def getMaxAmountOfNightsInInn(leftoverGold:float, people:int, horses:int) -> int:
+def getMaxAmountOfNightsInInn(leftoverGold: float, people:int, horses:int) -> int:
     pass
 
-def getJourneyInnCostsInGold(nightsInInn:int, people:int, horses:int) -> float:
+def getJourneyInnCostsInGold(nightsInInn: int, people:int, horses:int) -> float:
     pass
 
 ##################### M04.D02.O12 #####################
 
-def getInvestorsCuts(profitGold:float, investors:list) -> list:
+def getInvestorsCuts(profitGold: float, investors:list) -> list:
     pass
 
-def getAdventurerCut(profitGold:float, investorsCuts:list, fellowship:list) -> float:
+def getAdventurerCut(profitGold: float, investorsCuts:list, fellowship:list) -> float:
     pass
 
 ##################### M04.D02.O13 #####################
 
-def getEarnigs(profitGold:float, mainCharacter:dict, friends:list, investors:list) -> list:
+def getEarnigs(profitGold: float, mainCharacter:dict, friends:list, investors:list) -> list:
     pass
 
-def colored(text: str, color:any, attrs):
+def colored(text: str, color: any, attrs):
     print(text)
 
 
 ##################### view functions #####################
-def print_colorvars(txt:str='{}', vars:list=[], color:str='yellow') -> None:
-    vars = map(lambda string, color=color: colored(str(string), color, attrs=['bold']) ,vars)
+def print_colorvars(txt: str='{}', vars:list=[], color:str='yellow') -> None:
+    vars = map(lambda string, color=color: colored(str(string), color, attrs=['bold']) , vars)
     print(txt.format(*vars))
 
-def print_title(name:str) -> None:
+def print_title(name: str) -> None:
     print_colorvars(vars=['=== [ {} ] ==='.format(name)], color='green')
 
-def print_chapter(number:int, name:str) -> None:
+def print_chapter(number: int, name:str) -> None:
     nextStep(2)
-    print_colorvars(vars=['- CHAPTER {}: {} -'.format(number, name)], color='magenta')
+    print_colorvars(
+        vars=['- CHAPTER {}: {} -'.format(number, name)], color='magenta')
 
-def nextStep(secwait:int=1) -> None:
+def nextStep(secwait: int=1) -> None:
     print('')
     time.sleep(secwait)
 
-def ifOne(amount:int, yes:str, no:str, single='een') -> str:
+def ifOne(amount: int, yes:str, no:str, single='een') -> str:
     text = yes if amount == 1 else no
     amount = single if amount == 1 else amount
     return '{} {}'.format(amount, text).lstrip()
